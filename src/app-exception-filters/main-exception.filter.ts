af57859338e0afdbf81b03e4b1dc148128411687
Pawel <62116742+mapawel@users.noreply.github.com>
2023-04-24 16:18:04 +0200

    project init: nestjs with ES2022, clean app app-module only + MainExc… (#2)
    * project init: nestjs with ES2022, clean app app-module only + MainExceptionFilter + ValidationPipe + env
    
    * fix nestproject-init#001
    
    * feat: add custom logger
    
    * fix: make logger.error more simple
    
    * fix: add class to the logger definition
    
    * fix: change logger class to interface
    
    * fix: final for Logger in main exception
    
    ---------
    
    Co-authored-by: Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>

---
+++ src/app-exception-filters/main-exception.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  Logger,
+  LoggerService,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class MainExceptionFilter implements ExceptionFilter {
+  private readonly logger: LoggerService = new Logger(MainExceptionFilter.name);
+
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    this.logger.error(exception.stack);
+
+    response.status(status).json({
+      message:
+        status === 500
+          ? 'Ups... Something went wrong. Try again later.'
+          : exception.message,
+    });
+  }
+}
