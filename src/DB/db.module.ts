b2c15a69855e6871324209af6cf1f1a15b1b2358
mapawel <pawelma@gmail.com>
2023-04-24 17:48:26 +0200

    resolved las conflict + indent 4 formated

--- src/DB/db.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { dbProviders } from './providers/db.provider';
-
-@Module({
-  providers: [...dbProviders],
-  exports: [...dbProviders],
-})
-export class DBModule {}
--- src/app-exception-filters/main-exception.filter.ts
+++ src/app-exception-filters/main-exception.filter.ts
@@ -1,30 +1,32 @@
 import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException,
-  Logger,
-  LoggerService,
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    HttpException,
+    Logger,
+    LoggerService,
 } from '@nestjs/common';
 import { Response } from 'express';
 
 @Catch()
 export class MainExceptionFilter implements ExceptionFilter {
-  private readonly logger: LoggerService = new Logger(MainExceptionFilter.name);
+    private readonly logger: LoggerService = new Logger(
+        MainExceptionFilter.name,
+    );
 
-  catch(exception: Error, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const status =
-      exception instanceof HttpException ? exception.getStatus() : 500;
+    catch(exception: Error, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const status =
+            exception instanceof HttpException ? exception.getStatus() : 500;
 
-    this.logger.error(exception.stack);
+        this.logger.error(exception.stack);
 
-    response.status(status).json({
-      message:
-        status === 500
-          ? 'Ups... Something went wrong. Try again later.'
-          : exception.message,
-    });
-  }
+        response.status(status).json({
+            message:
+                status === 500
+                    ? 'Ups... Something went wrong. Try again later.'
+                    : exception.message,
+        });
+    }
 }
