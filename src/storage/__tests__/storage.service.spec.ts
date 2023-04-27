dbf9adfecc27b892595299a96ba98afa9387457a
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2023-04-27 21:27:24 +0200

    fix: some minor fixes, forced to wait until file saved

--- src/storage/__tests__/storage.service.spec.ts
+++ src/storage/__tests__/storage.service.spec.ts
@@ -1,24 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StorageService } from '../storage.service';
 import { ConfigService } from '@nestjs/config';
-import { Logger, StreamableFile } from '@nestjs/common';
 import { MainExceptionFilter } from 'src/app-exception-filters/main-exception.filter';
-import * as fs from 'fs';
-import { log } from 'console';
-import path from 'path';
+import mock = require('mock-fs');
+
 const mockResponse = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn(),
 };
 const mockRequest = {};
 
-jest.mock('uuid', () => ({ v4: jest.fn(() => 'id_name') }));
-
-jest.mock('fs', () => ({
-    createWriteStream: jest.fn(),
-    createReadStream: jest.fn(),
-    readdir: jest.fn(),
-}));
+jest.mock('uuid', () => ({ v4: jest.fn(() => 'test_id') }));
 
 describe('StorageService', () => {
     let service: StorageService;
@@ -33,13 +25,7 @@ describe('StorageService', () => {
                 {
                     provide: ConfigService,
                     useValue: {
-                        get: jest.fn(),
-                    },
-                },
-                {
-                    provide: Logger,
-                    useValue: {
-                        error: jest.fn(),
+                        get: jest.fn(() => 'src/storage/__tests__/test'),
                     },
                 },
             ],
@@ -52,71 +38,30 @@ describe('StorageService', () => {
         jest.clearAllMocks();
     });
 
+    afterAll(() => {
+        mock.restore();
+    });
+
     describe('readFile', () => {
-        // it('should return the content of the file', async () => {
-        //     //given
-        //     const fileName = 'id.html';
-        //     const contentInFile = 'Hello World';
-        //     jest.spyOn(config, 'get').mockReturnValueOnce(
-        //         'src/storage/__tests__/test',
-        //     );
-        //     jest.spyOn(fs, 'readdir').mockImplementation(() => [fileName]);
-        //     const mReadStream = {
-        //         pipe: jest.fn().mockReturnThis(),
-        //         on: jest.fn().mockImplementation(function (event, handler) {
-        //           handler();
-        //           return this;
-        //         }),
-        //       };
-        //     jest.spyOn(fs, 'createReadStream').mockReturnValueOnce(mReadStream);
+        it('should return the content of the file', async () => {
+            const fileName = 'test_id';
+            const contentInFile = 'Hello World';
+            mock({
+                'src/storage/__tests__/test': {
+                    [fileName]: contentInFile,
+                },
+            });
 
-        //     //when
-        //     const readStream: fs.ReadStream = await service.readFile(fileName);
-        //     const fileContents: string = await new Promise((resolve, reject) => {
-        //         let buffer = '';
-        //         readStream.on('data', (data: string) => {
-        //           buffer += data;
-        //         });
-        //         readStream.on('end', () => {
-        //           resolve(buffer);
-        //         });
-        //         readStream.on('error', (err: any) => {
-        //           reject(err);
-        //         });
-        //       });
+            const readStream = await service.readFile(fileName);
 
-        //     //then
-        //     expect(fileContents).toEqual(contentInFile);
-        // });
-
-        it('should throw error, when file not existing', async () => {
-            const fileName = 'notExistingFile.txt';
-            jest.spyOn(config, 'get').mockReturnValueOnce(
-                'src/storage/__tests__/test',
-            );
-            jest.spyOn(fs, 'readdir').mockImplementation(() => []);
-
-            //then
-            try {
-                await service.readFile(fileName);
-            } catch (err) {
-                filter.catch(err, {
-                    switchToHttp: () => ({
-                        getRequest: () => mockRequest,
-                        getResponse: () => mockResponse,
-                    }),
-                } as any);
-
-                expect(mockResponse.status).toHaveBeenCalledWith(500);
-                expect(mockResponse.json).toHaveBeenCalledWith({
-                    message: 'Ups... Something went wrong. Try again later.',
-                });
-            }
+            expect(readStream).toEqual(contentInFile);
         });
 
-        it('should throw error, when something wrong with env or config service', async () => {
-            const fileName = 'notExistingFile.txt';
-            jest.spyOn(config, 'get').mockReturnValueOnce(undefined);
+        it('should throw error, when file not existing', async () => {
+            const fileName = 'notExistingFile.html';
+            mock({
+                'src/storage/__tests__/test': {},
+            });
 
             //then
             try {
@@ -129,9 +74,9 @@ describe('StorageService', () => {
                     }),
                 } as any);
 
-                expect(mockResponse.status).toHaveBeenCalledWith(500);
+                expect(mockResponse.status).toHaveBeenCalledWith(404);
                 expect(mockResponse.json).toHaveBeenCalledWith({
-                    message: 'Ups... Something went wrong. Try again later.',
+                    message: 'File does not exist',
                 });
             }
         });
@@ -139,61 +84,33 @@ describe('StorageService', () => {
 
     describe('writeFile', () => {
         it('should write file successfully', async () => {
-            //given
-            const fileId = 'id_name';
+            // given
+            const fileId = 'test_id';
             const file = {
-                buffer: Buffer.from('testing'),
-                mimetype: 'application/html',
-            } as unknown as Express.Multer.File;
-            const writeStream: fs.WriteStream = {
-                write: jest.fn(),
-            } as unknown as fs.WriteStream;
-            jest.spyOn(config, 'get').mockImplementation(
-                () => 'src/storage/__tests__/test',
-            );
+                mimetype: 'text/html',
+                buffer: Buffer.from('file contents'),
+            } as Express.Multer.File;
 
-            jest.spyOn(fs, 'createWriteStream').mockReturnValue(writeStream);
+            mock({
+                'src/storage/__tests__/test': {},
+            });
 
-            //when
-            const result: string = await service.saveFile(file);
+            // when
+            const result = await service.saveFile(file);
 
-            //then
+            // then
             expect(result).toEqual(fileId);
-        });
-
-        it('should throw InternalServerErrorException when storage not existing', async () => {
-            //given
-            const file = {
-                buffer: Buffer.from('testing'),
-            } as unknown as Express.Multer.File;
-            jest.spyOn(config, 'get').mockImplementation(() => undefined);
-
-            //then
-            try {
-                await service.saveFile(file);
-            } catch (err) {
-                filter.catch(err, {
-                    switchToHttp: () => ({
-                        getRequest: () => mockRequest,
-                        getResponse: () => mockResponse,
-                    }),
-                } as any);
-
-                expect(mockResponse.status).toHaveBeenCalledWith(500);
-                expect(mockResponse.json).toHaveBeenCalledWith({
-                    message: 'Ups... Something went wrong. Try again later.',
-                });
-            }
         });
 
         it('should throw InternalServerErrorException when something goes wrong with writhing the file', async () => {
             //given
             const file = {
-                buffer: Buffer.from('testing'),
-            } as unknown as Express.Multer.File;
-            jest.spyOn(config, 'get').mockImplementation(() => undefined);
-            jest.spyOn(fs, 'createWriteStream').mockImplementation(() => {
-                throw new Error('Can not write the file');
+                mimetype: 'text/html',
+                buffer: Buffer.from('file contents'),
+            } as Express.Multer.File;
+
+            jest.spyOn(service, 'saveFile').mockImplementation(() => {
+                throw new Error('Something goes wrong with saving the file');
             });
 
             //then
--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,72 +1,78 @@
-import {
-    Injectable,
-    InternalServerErrorException,
-    Logger,
-    LoggerService,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
-    ReadStream,
-    WriteStream,
     createReadStream,
     createWriteStream,
+    promises as fsPromises,
+    write,
 } from 'fs';
-import { readdir } from 'fs/promises';
 import { join } from 'path';
+import { Stream } from 'stream';
+import { promisify } from 'util';
 import { v4 as uuid } from 'uuid';
-import * as fs from 'fs';
+
+const pipeline = promisify(Stream.pipeline);
 
 @Injectable()
 export class StorageService {
-    constructor(private readonly configService: ConfigService) {}
+    private readonly storage: string | undefined;
+    private readonly html = 'html';
+    private readonly xml = 'xml';
 
-    async saveFile(file: Express.Multer.File): Promise<string> {
-        const storage: string | undefined =
-            this.configService.get<string>('STORAGE');
-
-        if (!storage) {
+    constructor(private readonly configService: ConfigService) {
+        this.storage = this.configService.get<string>('STORAGE');
+        if (!this.storage) {
             throw new Error('Something wrong with env or config service');
         }
+    }
 
+    saveFile = async (file: Express.Multer.File): Promise<string> => {
         const fileId: string = uuid();
-        const fileExtension: string = file.mimetype.includes('html')
-            ? 'html'
-            : 'xml';
-        const writeStream: WriteStream = createWriteStream(
-            join(process.cwd(), `${storage}/${fileId}.${fileExtension}`),
+        const fileExtension: string = file.mimetype.includes(this.html)
+            ? this.html
+            : this.xml;
+
+        const filePath = join(
+            process.cwd(),
+            `${this.storage}/${fileId}.${fileExtension}`,
         );
-        writeStream.write(file);
+
+        const writeableStream = createWriteStream(filePath);
+
+        writeableStream.write(file.buffer);
+        writeableStream.end();
 
         return fileId;
-    }
+    };
 
-    async readFile(name: string): Promise<ReadStream> {
-        const storage: string | undefined =
-            this.configService.get<string>('STORAGE');
+    readFile = async (name: string): Promise<string> => {
+        const dir: string = join(process.cwd(), `${this.storage}`);
+        const files = await fsPromises.readdir(dir);
 
-        if (!storage) {
-            throw new Error('Something wrong with env or config service');
+        const foundFile = files.find((file) => file.includes(name));
+
+        if (!foundFile) {
+            throw new NotFoundException('File does not exist');
         }
 
-        let filePath: string = '';
-        const dir: string = join(process.cwd(), `${storage}`);
-        const files = await readdir(dir);
+        const fullPath = join(dir, foundFile);
 
-        for (const file of files) {
-            if (file.includes(name)) {
-                filePath = join(dir, name);
-                break;
-            }
-        }
+        return this.getFileContent(fullPath);
+    };
 
-        if (filePath === '') {
-            throw new Error('File does not exist');
-        }
-
+    private getFileContent = (fullPath: string): Promise<string> => {
         return new Promise((resolve, reject) => {
-            const readStream = fs.createReadStream(filePath);
-            readStream.on('error', (error) => reject(error));
-            readStream.on('end', () => resolve(readStream));
+            const readStream = createReadStream(fullPath, 'utf-8');
+            let content = '';
+            readStream.on('data', (chunk) => {
+                content += chunk;
+            });
+            readStream.on('error', (error) => {
+                reject(error);
+            });
+            readStream.on('end', () => {
+                resolve(content);
+            });
         });
-    }
+    };
 }
