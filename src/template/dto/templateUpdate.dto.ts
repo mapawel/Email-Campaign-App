39a4013099c2f17f87b869ead6614ae420be3165
mapawel <pawelma@gmail.com>
2023-05-08 21:19:45 +0200

    feat: templateUpdateDTO

--- src/template/dto/templateUpdate.dto.ts
+++ src/template/dto/templateUpdate.dto.ts
@@ -1,4 +1,24 @@
 import { TemplateCreateDTO } from './templateCreate.dto';
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, IsDateString, Length } from 'class-validator';
+import validators from '../../validation/settings/validators.json';
+import { buildValidatorKeyParamsGetter } from '../../validation/utils';
 
-export class TemplateUpdateDTO extends PartialType(TemplateCreateDTO) {}
+const getTemplateValidatorParam =
+    buildValidatorKeyParamsGetter<TemplateCreateDTO>({
+        entityName: 'template',
+        validatorsObject: validators,
+    });
+
+export class TemplateUpdateDTO extends PartialType(TemplateCreateDTO) {
+    @IsDateString()
+    updatedAt: Date;
+
+    @IsString()
+    @Length(
+        getTemplateValidatorParam('createdBy', 'minLength') || 4,
+        getTemplateValidatorParam('createdBy', 'maxLength') || 36,
+        // getTemplateValidatorParam('', 'maxLength') || 36,
+    )
+    updatedBy: string;
+}
