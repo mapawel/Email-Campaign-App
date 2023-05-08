b1f4fe0af71e13ae09b5710100048feb782123b1
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2023-05-08 20:19:21 +0200

    fix: fixing test for storage

--- src/email-provider/email-provider.module.ts
+++ src/email-provider/email-provider.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { EmailProviderService } from './email-provider.service';
 
 @Module({
-  providers: [EmailProviderService]
+    providers: [EmailProviderService],
 })
 export class EmailProviderModule {}
--- src/email-provider/email-provider.service.spec.ts
+++ src/email-provider/email-provider.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EmailProviderService } from './email-provider.service';
 
 describe('EmailProviderService', () => {
-  let service: EmailProviderService;
+    let service: EmailProviderService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [EmailProviderService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [EmailProviderService],
+        }).compile();
 
-    service = module.get<EmailProviderService>(EmailProviderService);
-  });
+        service = module.get<EmailProviderService>(EmailProviderService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
