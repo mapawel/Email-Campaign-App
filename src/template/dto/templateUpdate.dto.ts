39410baf4654fec10c09f04a19b92a4054f1b8da
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2023-05-25 18:08:56 +0200

    fix: resolve conflicts

--- src/template/dto/templateUpdate.dto.ts
+++ src/template/dto/templateUpdate.dto.ts
@@ -1,4 +1,4 @@
-import { TemplateCreateDTO } from './templateCreate.dto';
 import { PartialType } from '@nestjs/mapped-types';
+import { TemplateCreateDTO } from './templateCreate.dto';
 
 export class TemplateUpdateDTO extends PartialType(TemplateCreateDTO) {}
